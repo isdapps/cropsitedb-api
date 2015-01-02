64b1efd60abcd8ad48a3ae0dd85f851d036e6fe1
Christopher Villalobos <christopher.villalobos@gmail.com>
2015-01-02 12:10:43 -0500

    Tweaking DOME Processing

--- app/actors/ProcessDOME.scala
+++ app/actors/ProcessDOME.scala
@@ -10,23 +10,31 @@ import java.util.zip.GZIPInputStream
 
 import com.fasterxml.jackson.core.{JsonFactory, JsonParser, JsonToken}
 
+import play.api.db.DB
+import anorm._
+import cropsitedb.helpers.{AnormHelper, GeoHashHelper,CropsiteDBConfig}
+
+import play.api.Play.current
+
 class ProcessDOME extends Actor with ActorLogging {
+
 	val factory = new JsonFactory()
 
 	def receive = {
-	  case msg: Messages.ProcessFile => {
-	    processing(msg)
-	  }
-  }
+		case msg: Messages.ProcessFile => {
+			processing(msg)
+		}
+	}
 
 	def processing(msg: Messages.ProcessFile) = {
 		log.info("FIRING!!!")
-    val domeFile = new File(msg.filePath)
-    if( domeFile.exists ) {
-      log.info("File exists")
-    } else {
-      log.info("File does not exist")
-    }
+		val domeFile = new File(msg.filePath)
+		if( domeFile.exists ) {
+			log.info("File exists")
+			processDome(domeFile)
+		} else {
+			log.info("File does not exist")
+		}
 	}
 
 	def processDome(f: File) {
@@ -34,6 +42,34 @@ class ProcessDOME extends Actor with ActorLogging {
 		val gis = new GZIPInputStream(fis)
 		val jp = factory.createParser(gis)
 		try {
+			var level: Int = 0
+			var currentDome: String = ""
+			// Seek to info
+			while(Option(jp.nextToken()).isDefined) {
+				jp.getCurrentToken match {
+					case JsonToken.START_OBJECT => {
+						level = level + 1
+						level match {
+							case 1 => {
+								jp.nextToken
+								currentDome = jp.getCurrentName
+								log.info("Found DOME: "+currentDome)
+							}
+							case _ => {
+								jp.getCurrentName match {
+									case "info" => parseDomeInfo(jp, currentDome)
+									case _ => {}
+
+								}
+							}
+						}
+					}
+					case JsonToken.END_OBJECT => {
+						level = level - 1
+					}
+					case _ => {}
+				}
+			}
 		} catch {
 			case _ : Throwable => {}
 		} finally {
@@ -41,5 +77,10 @@ class ProcessDOME extends Actor with ActorLogging {
 			gis.close
 			fis.close
 		}
+	}
+
+	def parseDomeInfo(p: JsonParser, name: String) {
+		log.info("Launching DOME Info Parser for "+name)
+		log.info("STRING: "+p.getValueAsString())
 	}
 }
