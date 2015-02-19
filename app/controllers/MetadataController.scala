5acfcd36adf1eb0dcfc1383759cec88585da6cd2
Christopher Villalobos <cvillalobos@ufl.edu>
2015-02-19 12:34:38 -0500

    Tweaked bulk_loading from hash

--- app/controllers/MetadataController.scala
+++ app/controllers/MetadataController.scala
@@ -1,5 +1,8 @@
 package cropsitedb.controllers
 
+import java.util.Calendar
+import java.util.Date
+
 import anorm._
 import play.api._
 import play.api.db.DB
@@ -30,8 +33,31 @@ object MetadataController extends Controller {
             case None => BadRequest(Json.obj("error" -> "Invalid query"))
             case Some(siteRow) => {
               val site = siteRow[String]("site")
-              
-              Ok(site)
+              val lastQ = SQL("SELECT last_seen FROM bulk_loading WHERE site={qSite}").on('qSite -> site).apply.headOption
+              val now = new Date()
+              val cal = Calendar.getInstance()
+              val check = lastQ match {
+                case None => {
+                  val insertResult = SQL("INSERT INTO bulk_loading VALUES ({qSite}, {qDate})").on('qSite -> site, 'qDate -> now).execute()
+                  cal.set(Calendar.YEAR, 1981)
+                  cal.set(Calendar.MONTH, Calendar.JULY)
+                  cal.set(Calendar.DAY_OF_MONTH, 6)
+                  cal
+                }
+                case Some(lastRow) => {
+                  val last = lastRow[Date]("last_seen")
+                  cal.setTime(last)
+                  val updateResult = SQL("UPDATE bulk_loading SET last_seen = {qDate} WHERE site = {qSite}").on('qSite -> site, 'qDate -> now).execute()
+                  cal
+                }
+              }
+              check.set(Calendar.HOUR_OF_DAY, 0)
+              check.set(Calendar.MINUTE, 0)
+              check.set(Calendar.SECOND, 0)
+              check.set(Calendar.MILLISECOND, 1)
+              check.add(Calendar.DAY_OF_MONTH, -1)
+              val q = SQL("SELECT * FROM ace_metadata WHERE created > {qDate}").on('qDate -> new Date(cal.getTimeInMillis())).apply
+              Ok(JsonHelper.structureQueryOutput(q))
             }
           }
         }
